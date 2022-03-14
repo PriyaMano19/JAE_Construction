c602928721cfe18ecfb6faf1b03c538f1c1c8790
priya_19 <it20141042@my.sliit.lk>
2022-03-14 12:55:52 +0530

    first commit

---
+++ resources/views/front/employee.blade.php
@@ -0,0 +1,110 @@
+@extends('layouts.master')
+@section('content')
+<button class="btn-plus" data-toggle="modal" data-target="#myModal"><i class="fa fa-plus fa-2xl"></i></button>
+
+      <!-- Modal -->
+      <div style="margin-top: 60px;" id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+          <!-- Modal content-->
+          <div class="modal-content">
+            <div class="modal-header" style="color:white;background-color: #B2022F;">
+            <div style="flex-direction:column;">
+              <h4 class="text-center modal-title" style="color:white">ADD Employee</h4>
+             
+            </div>
+            </div>
+
+            <div class="modal-body">
+              <div id="error">
+                
+              </div>
+              <form name="book_form" action="layout/insert-booking.php" method="post" onsubmit="return validateForm()">
+                
+                <div class="m-b-10">
+                  <input class="form-control" type="text" name="owner_name" placeholder="Your Name">
+                </div>
+                <div class="m-b-10">
+                  <input class="form-control" type="text" name="vehicle_no" placeholder="Vehicle Number">
+                </div>
+
+                <div class="m-b-10">
+                  <input class="form-control" type="text" name="contact_no" placeholder="Mobile Number">
+                  <span id="mobile_error"></span>
+                </div>
+
+                
+
+                  
+              
+            </div>
+            <div class="modal-footer">
+              <button style="border-radius: 0px;" type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+              <input type="submit" name="submit" style="border-radius: 0px;" class="btn btn-dark" value="Save">
+            </div>
+            </form>
+          </div>
+
+        </div>
+      </div>
+
+<div class="container-fluid py-4">
+      <div class="row">
+        <div class="col-12">
+          <div class="card my-4">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+              <div class="bg-danger shadow-primary border-radius-lg pt-4 pb-3">
+                <h6 class="text-white text-capitalize ps-3">Employee details</h6>
+              </div>
+            </div>
+            <div class="card-body px-0 pb-2">
+              <div class="table-responsive p-0">
+                <table class="table align-items-center mb-0">
+                  <thead>
+                    <tr>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-15">Employee_ID</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-15 ps-2">Name</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-15">Contact_N0</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-15">NIC</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-15">Skills</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-15">Amount</th>
+                      <th class="text-secondary opacity-7">Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>
+                       
+                      </td>
+                      <td>
+                       
+                      </td>
+                      <td class="align-middle text-center text-sm">
+                       
+                      </td>
+                      <td class="align-middle text-center">
+                       
+                      </td>
+                      <td class="align-middle">
+                        
+                          
+                      </td>
+                      <td class="align-middle">
+                        
+                          
+                      </td>
+                      <td class="align-middle">
+                        <a href="" class="btn btn-success "><i class="fa fa-pencil"></i></a>
+                          <a href="" class="btn btn-warning "><i class="fa fa-eye "></i></a>
+                          
+                      </td>
+                    </tr>
+                   
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>JAE</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Nucleo Icons -->
+  <link href="{{asset('css/nucleo-icons.css')}}" rel="stylesheet" />
+  <link href="{{asset('css/nucleo-svg.css')}}" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <!-- Material Icons -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+  <!-- CSS Files -->
+  <link id="pagestyle" href="{{asset('css/material-dashboard.css?v=3.0.1')}}" rel="stylesheet" />
+		
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="{{asset('css/style.css')}}">
+  </head>
+  <body>
+		
+		<div class="wrapper d-flex align-items-stretch">
+			<nav id="sidebar">
+				<div class="custom-menu">
+					<button type="button" id="sidebarCollapse" class="btn btn-primary">
+	          <i class="fa fa-bars"></i>
+	          <span class="sr-only">Toggle Menu</span>
+	        </button>
+        </div>
+        <img class="logo-img" src="{{('img/LOGO.png')}}" alt="Paris">
+	  		<h1><a href="index.html" class="logo">JAE Construction</a></h1>
+        <ul class="list-unstyled components mb-5">
+          <li class="active">
+            <a href="#"><span class="fa fa-home mr-3"></span> Homepage</a>
+          </li>
+          <li>
+              <a href="#"><span class="fa fa-user mr-3"></span> Employee</a>
+          </li>
+          <li>
+            <a href="#"><span class="fa fa-sticky-note mr-3"></span> Category</a>
+          </li>
+          <li>
+            <a href="#"><span class="fa fa-sticky-note mr-3"></span> Items</a>
+          </li>
+          <li>
+            <a href="#"><span class="fa fa-paper-plane mr-3"></span> Project</a>
+          </li>
+          <li>
+            <a href="#"><span class="fa fa-paper-plane mr-3"></span> Project Budget</a>
+          </li>
+        </ul>
+
+    	</nav>
+
+        <!-- Page Content  -->
+      <div id="content" class="p-4 p-md-5 pt-5">
+      <div class='container'>
+            @yield('content')
+          </div>
+      </div>
+		</div>
+
+    <script src="{{asset('js/jquery.min.js')}}"></script>
+    <script src="{{asset('js/popper.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/main.js')}}"></script>
+  </body>
+</html>
